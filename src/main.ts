87e1dbf9671bf2c5014a86b136b6f939bc1e5c9d
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-06-03 13:06:33 -0300

    feat: buscarPokemon

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log("Funcionando")
+import { buscarPokemon } from "./services/PokeApiService";
+
+async function main() {
+    const pikachu = await buscarPokemon('pikachu');
+    console.log(pikachu);
+}
+
+main();
