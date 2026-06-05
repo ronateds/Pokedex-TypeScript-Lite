14b579e272d73eb042d73a35ede5ea06ba0a9186
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-06-05 14:08:23 -0300

    refactor: remoção de código redundante

--- src/controllers/TerminalController.ts
+++ src/controllers/TerminalController.ts
@@ -6,19 +6,9 @@ import { buscarPokemon } from "../services/PokeApiService";
 export async function adicionarPokemon(catalogo: Catalogo, pokemon: string): Promise<void> {
     const pokemonObj: PokemonResumo | null =
         catalogo.encontrar(pokemon) ??
-        await buscarPokenonNaAPI(pokemon);
+        await buscarPokemon(pokemon);
 
     if (pokemonObj) adicionarAoCatalogo(catalogo, pokemonObj);
-}
-
-export async function buscarPokenonNaAPI(pokemon: string): Promise<PokemonResumo | null>{
-    return new Promise(async (resolve) => {
-        const _pokemon: PokemonResumo | null = await buscarPokemon(pokemon);
-
-        if(!_pokemon) resolve(null);
-
-        resolve(_pokemon);
-    })
 }
 
 export function adicionarAoCatalogo(catalogo: Catalogo, pokemon: PokemonResumo): void {
