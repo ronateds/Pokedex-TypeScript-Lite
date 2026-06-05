1355e6f7d2d1dcfcd705c15d24715934942fa621
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-06-05 03:38:26 -0300

    feat: adicionei contoller na main.ts e removi o acesso direto a service.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
-import { buscarPokemon } from "./services/PokeApiService";
-
-async function main() {
-    const pikachu = await buscarPokemon('pikachu');
-    console.log(pikachu);
-}
+import { PokemonResumo } from "./models/Pokemon";
+import * as controller from "./controllers/TerminalController";
 
 main();
+
+async function main() {
+    let catalogo: PokemonResumo[] = [];
+
+    /*
+        Busca pokemon no catalogo local,
+        se não existir, busca na PokeAPI
+        e adiciona ao catalogo.
+    */
+    let pikachuString: string = 'pikachu';
+    let pikachuObj: PokemonResumo | null =
+        catalogo.find(item => item.nome == pikachuString) ??
+        await controller.buscarPokenonNaAPI(pikachuString);
+    if(pikachuObj) controller.adicionarAoCatalogo(catalogo, pikachuObj);
+
+    // Simula adicionar um pokemon duplicado
+    let pikachu2String: string = 'pikachu';
+    let pikachu2Obj: PokemonResumo | null =
+        catalogo.find(item => item.nome == pikachuString) ??
+        await controller.buscarPokenonNaAPI(pikachu2String);
+    if (pikachu2Obj) controller.adicionarAoCatalogo(catalogo, pikachu2Obj);
+
+    let charmanderString: string = 'charmander';
+    let charmanderObj: PokemonResumo | null =
+        catalogo.find(item => item.nome == charmanderString) ??
+        await controller.buscarPokenonNaAPI(charmanderString);
+    if (charmanderObj) controller.adicionarAoCatalogo(catalogo, charmanderObj);
+
+    controller.listarCatalogo(catalogo);
+}
