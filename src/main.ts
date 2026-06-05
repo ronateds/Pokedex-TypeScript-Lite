1e05ca52fa2ca28d10cdfb925066789f5bec65f1
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-06-05 13:32:25 -0300

    refactor: mudei adicionarPokemon para o TerminalController

--- src/main.ts
+++ src/main.ts
@@ -3,36 +3,18 @@ import * as controller from "./controllers/TerminalController";
 
 main();
 
-// TODO refact - muito código repetido
 async function main() {
     let catalogo: PokemonResumo[] = [];
 
-    /*
-        Busca pokemon no catalogo local,
-        se não existir, busca na PokeAPI
-        e adiciona ao catalogo.
-    */
-    let pikachuString: string = 'pikachu';
-    let pikachuObj: PokemonResumo | null =
-        catalogo.find(item => item.nome == pikachuString) ??
-        await controller.buscarPokenonNaAPI(pikachuString);
-    if(pikachuObj) controller.adicionarAoCatalogo(catalogo, pikachuObj);
+    controller.listarCatalogo(catalogo);
 
-    // Simula adicionar um pokemon duplicado
-    let pikachu2String: string = 'pikachu';
-    let pikachu2Obj: PokemonResumo | null =
-        catalogo.find(item => item.nome == pikachuString) ??
-        await controller.buscarPokenonNaAPI(pikachu2String);
-    if (pikachu2Obj) controller.adicionarAoCatalogo(catalogo, pikachu2Obj);
-
-    // Adicionar charmander
-    let charmanderString: string = 'charmander';
-    let charmanderObj: PokemonResumo | null =
-        catalogo.find(item => item.nome == charmanderString) ??
-        await controller.buscarPokenonNaAPI(charmanderString);
-    if (charmanderObj) controller.adicionarAoCatalogo(catalogo, charmanderObj);
+    await controller.adicionarPokemon(catalogo, 'pikachu');
+    await controller.adicionarPokemon(catalogo, 'pikachu');
+    await controller.adicionarPokemon(catalogo, 'charmander');
+    await controller.adicionarPokemon(catalogo, '42');
+    await controller.adicionarPokemon(catalogo, 'pokemon-inexistente');
 
     controller.listarCatalogo(catalogo);
     // controller.removerDoCatalogo(catalogo, '25'); // TODO
-    controller.listarCatalogo(catalogo);
+    // controller.listarCatalogo(catalogo);
 }
