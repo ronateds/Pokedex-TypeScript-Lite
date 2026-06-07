6d5d4eb63557e43a18aaa64f16e70b77506500e4
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-06-07 08:47:51 -0300

    refactor: converti PokeApiService e TerminalController para uma classe e transferi chamdas para o TerminalController

--- src/controllers/TerminalController.ts
+++ src/controllers/TerminalController.ts
@@ -1,24 +1,45 @@
 import { CatalogoPokemon } from "../models/CatalogoPokemon";
 import { PokemonResumo } from "../models/Pokemon";
-import { buscarPokemon } from "../services/PokeApiService";
+import { PokeApiService } from "../services/PokeApiService";
 
-// Busca pokemon no catalogo local, se não existir, busca na PokeAPI e adiciona ao catalogo.
-export async function adicionarPokemon(catalogo: CatalogoPokemon, pokemon: string): Promise<void> {
-    const pokemonObj: PokemonResumo | null =
-        catalogo.encontrar(pokemon) ??
-        await buscarPokemon(pokemon);
+export class TerminalController {
+    constructor(
+        private readonly api: PokeApiService,
+        private readonly catalogo: CatalogoPokemon
+    ) {}
 
-    if (pokemonObj) adicionarAoCatalogo(catalogo, pokemonObj);
-}
+    public async start(): Promise<void> {
+        this.listarCatalogo(this.catalogo);
 
-export function adicionarAoCatalogo(catalogo: CatalogoPokemon, pokemon: PokemonResumo): void {
-    catalogo.adicionar(pokemon)
-}
+        await this.adicionarPokemon(this.catalogo, 'pikachu');
+        await this.adicionarPokemon(this.catalogo, 'pikachu');
+        await this.adicionarPokemon(this.catalogo, 'charmander');
+        await this.adicionarPokemon(this.catalogo, '42');
+        await this.adicionarPokemon(this.catalogo, 'pokemon-inexistente');
 
-export function listarCatalogo(catalogo: CatalogoPokemon): void {
-    catalogo.listar()
-}
+        this.listarCatalogo(this.catalogo);
+        this.removerDoCatalogo(this.catalogo, 25);
+        this.listarCatalogo(this.catalogo);
+    }
 
-export function removerDoCatalogo(catalogo: CatalogoPokemon, pokemonId: number): void {
-    catalogo.remover(pokemonId)
+    // Busca pokemon no catalogo local, se não existir, busca na PokeAPI e adiciona ao catalogo.
+    private async adicionarPokemon(catalogo: CatalogoPokemon, pokemon: string): Promise<void> {
+        const pokemonObj: PokemonResumo | null =
+            catalogo.encontrar(pokemon) ??
+            await this.api.buscarPokemon(pokemon);
+
+        if (pokemonObj) this.adicionarAoCatalogo(catalogo, pokemonObj);
+    }
+
+    adicionarAoCatalogo(catalogo: CatalogoPokemon, pokemon: PokemonResumo): void {
+        catalogo.adicionar(pokemon)
+    }
+
+    listarCatalogo(catalogo: CatalogoPokemon): void {
+        catalogo.listar()
+    }
+
+    removerDoCatalogo(catalogo: CatalogoPokemon, pokemonId: number): void {
+        catalogo.remover(pokemonId)
+    }
 }
--- src/services/PokeApiService.ts
+++ src/services/PokeApiService.ts
@@ -1,30 +1,38 @@
 import { PokemonApiResponse, PokemonResumo } from '../models/Pokemon';
 
-export async function buscarPokemon(nomeOuId: string): Promise<PokemonResumo | null> {
-    return new Promise(async (resolve) => {
-        try {
-            const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${ nomeOuId }`);
+export class PokeApiService {
+    private readonly baseUrl: string;
 
-            if (!resposta.ok) {
-                console.log(`[ERRO] Pokémon não encontrado: ${ nomeOuId }`);
+    constructor(baseUrl: string = 'https://pokeapi.co/api/v2/pokemon/' ) {
+        this.baseUrl = baseUrl
+    }
+
+    public async buscarPokemon(nomeOuId: string): Promise<PokemonResumo | null> {
+        return new Promise(async (resolve) => {
+            try {
+                const resposta = await fetch(`${ this.baseUrl + nomeOuId }`);
+
+                if (!resposta.ok) {
+                    console.log(`[ERRO] Pokémon não encontrado: ${ nomeOuId }`);
+                    resolve(null);
+                    return;
+                }
+
+                const dados: PokemonApiResponse = await resposta.json();
+
+                const pokemon: PokemonResumo = {
+                    id: dados.id,
+                    nome: dados.name,
+                    tipos: dados.types.map((item: any) => item.type.name),
+                    altura: dados.height,
+                    peso: dados.weight
+                };
+
+                resolve(pokemon);
+            } catch (erro) {
+                console.log("[ERRO] Não foi possível buscar o Pokémon.");
                 resolve(null);
-                return;
             }
-
-            const dados: PokemonApiResponse = await resposta.json();
-
-            const pokemon: PokemonResumo = {
-                id: dados.id,
-                nome: dados.name,
-                tipos: dados.types.map((item: any) => item.type.name),
-                altura: dados.height,
-                peso: dados.weight
-            };
-
-            resolve(pokemon);
-        } catch (erro) {
-            console.log("[ERRO] Não foi possível buscar o Pokémon.");
-            resolve(null);
-        }
-    });
+        });
+    }
 }
